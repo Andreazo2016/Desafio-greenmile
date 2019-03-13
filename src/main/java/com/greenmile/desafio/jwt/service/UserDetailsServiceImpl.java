bb681e66dc1f73f6e1a1d976e8d3ba43f4f85462
Andreazo Silva Souza <andreazo2012@gmail.com>
2019-03-13 20:45:42 -0300

    Configuração JWT terminado

---
+++ src/main/java/com/greenmile/desafio/jwt/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.greenmile.desafio.jwt.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service(value = "myUserServiceDetails")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return User.withUsername("admin")
+                .password( passwordEncoder.encode( "admin") )
+                .authorities("admin")
+                .build();
+
+    }
+
+}
